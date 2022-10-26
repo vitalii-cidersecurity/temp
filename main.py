df5ef0d759a7004240ce0343fe56f2cca6daeb1c
Vitalii Trofymenko <vitali@cidersecurity.io>
2022-10-26 21:42:03 +0100

    main

---
+++ main.py
@@ -0,0 +1,14 @@
+import flask
+import os
+import connexion
+
+def create_app(app_name='MYAPPNAME'):
+    flask_app = connexion.App(__name__, skip_error_handlers=False)
+    return flask_app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080))) # nosemgrep
+
+else:
+    pass
